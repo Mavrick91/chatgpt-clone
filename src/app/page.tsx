39cc710c8d7cfb79252fbe79fe8b4b3fecf573ee
Mavrick91 <ray.mavrick@gmail.com>
2024-05-16 09:44:37 +0800

    Update eslint configuration to include tailwindcss plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import GPTLogo from "@/components/svg/GPTLogo";
 
 export default function Home() {
 	return (
-		<section className="h-full flex justify-center items-center">
+		<section className="flex h-full items-center justify-center">
 			<div className="flex h-full flex-col items-center justify-center text-token-text-primary">
 				<div className="relative">
-					<div className="mb-3 h-12 w-12">
-						<div className="relative flex h-full items-center justify-center rounded-full bg-token-main-surface-primary text-token-text-primary">
+					<div className="mb-3 size-12">
+						<div className="relative flex h-full items-center justify-center rounded-full bg-token-main-surface-primary text-token-text-primary after:absolute after:inset-0 after:rounded-full after:border after:border-gray-600 after:p-2 after:content-['']">
 							<GPTLogo />
 						</div>
 					</div>
