64d17dd3fc7654b8bdc9c0368d984fb1f3cfdad9
Mavrick91 <ray.mavrick@gmail.com>
2024-05-17 16:09:20 +0800

    Display spinner when chats are loading (sidebar)

--- src/app/_components/SideBar/SideBarConversation/index.tsx
+++ src/app/_components/SideBar/SideBarConversation/index.tsx
@@ -5,6 +5,7 @@ import { collection, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import ChatRow from "./ChatRow";
+import Spinner from "@/components/Spinner";
 
 const SideBarConversation = () => {
 	const { data: session } = useSession();
@@ -14,11 +15,15 @@ const SideBarConversation = () => {
 	return (
 		<div className="-mr-2 flex-1 flex-col overflow-y-auto pr-2 transition-opacity duration-500">
 			<div className="juice:mt-5 flex flex-col gap-2 pb-2 text-sm text-token-text-primary">
-				<ol>
-					{chats?.docs.map((doc) => (
-						<ChatRow key={doc.id} id={doc.id} />
-					))}
-				</ol>
+				{loading ? (
+					<Spinner />
+				) : (
+					<ol>
+						{chats?.docs.map((doc) => (
+							<ChatRow key={doc.id} id={doc.id} />
+						))}
+					</ol>
+				)}
 			</div>
 		</div>
 	);
