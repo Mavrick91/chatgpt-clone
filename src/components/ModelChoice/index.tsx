5b46bc87ef4bce524736af31bb0e08853b0f80fd
Mavrick91 <ray.mavrick@gmail.com>
2024-05-17 16:11:09 +0800

    Limit dropdown height model choice

--- src/components/ModelChoice/index.tsx
+++ src/components/ModelChoice/index.tsx
@@ -51,27 +51,29 @@ const ModelChoice = () => {
 						<div className="mb-1 px-5 pb-0 pr-4 pt-2">
 							<span className="text-sm text-token-text-tertiary">Model</span>
 						</div>
-						{models.map((model) => (
-							<button
-								onClick={() => handleUpdateModel(model.id)}
-								key={model.id}
-								role="menuitem"
-								className="group relative m-1.5 mx-2 my-0 flex grow cursor-pointer items-center gap-2.5 rounded-md p-3 !pr-3 text-sm hover:bg-[#f5f5f5] focus-visible:bg-[#f5f5f5] focus-visible:outline-0 dark:hover:bg-token-main-surface-secondary dark:focus-visible:bg-token-main-surface-secondary"
-								tabIndex={-1}
-							>
-								<div className="flex grow items-center justify-between gap-2">
-									<div>
-										<div className="flex items-center gap-3">
-											<span className="flex size-7 shrink-0 items-center justify-center rounded-full bg-token-main-surface-secondary">
-												<BigModelIcon />
-											</span>
-											<div>{model.id}</div>
+						<div className="flex max-h-72 flex-col overflow-y-auto">
+							{models.map((model) => (
+								<button
+									onClick={() => handleUpdateModel(model.id)}
+									key={model.id}
+									role="menuitem"
+									className="group relative m-1.5 mx-2 my-0 flex grow cursor-pointer items-center gap-2.5 rounded-md p-3 !pr-3 text-sm hover:bg-[#f5f5f5] focus-visible:bg-[#f5f5f5] focus-visible:outline-0 dark:hover:bg-token-main-surface-secondary dark:focus-visible:bg-token-main-surface-secondary"
+									tabIndex={-1}
+								>
+									<div className="flex grow items-center justify-between gap-2">
+										<div>
+											<div className="flex items-center gap-3">
+												<span className="flex size-7 shrink-0 items-center justify-center rounded-full bg-token-main-surface-secondary">
+													<BigModelIcon />
+												</span>
+												<div>{model.id}</div>
+											</div>
 										</div>
+										{model.id === defaultModel && <CheckMarkIcon />}
 									</div>
-									{model.id === defaultModel && <CheckMarkIcon />}
-								</div>
-							</button>
-						))}
+								</button>
+							))}
+						</div>
 					</div>
 				</div>
 			)}
