046f6dc27929286deef707dfd63cc66f22c830df
Mavrick91 <ray.mavrick@gmail.com>
2024-05-18 15:02:20 +0800

    Adjust styling

--- src/app/_components/SideBar/SideBarConversation/index.tsx
+++ src/app/_components/SideBar/SideBarConversation/index.tsx
@@ -16,7 +16,7 @@ const SideBarConversation = () => {
 
 	return (
 		<div className="-mr-2 flex-1 flex-col overflow-y-auto pr-2 transition-opacity duration-500">
-			<div className="mt-5 flex flex-col gap-2 pb-2 text-sm text-token-text-primary">
+			<div className="flex flex-col gap-2 pb-2 text-sm text-token-text-primary">
 				{
 					<ol>
 						{conversations.map((conversation) => (
--- src/app/_components/SideBar/SideBarHeader/index.tsx
+++ src/app/_components/SideBar/SideBarHeader/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import CollapseIcon from "@/components/svg/CollapseIcon";
 import NewIcon from "@/components/svg/NewIcon";
 import { useRouter } from "next/navigation";
 
@@ -8,10 +7,7 @@ const SideBarHeader = () => {
 	const router = useRouter();
 
 	return (
-		<div className="flex h-14 items-center justify-between">
-			<button className="h-10 rounded-lg px-2.5 text-token-text-secondary hover:bg-token-sidebar-surface-secondary focus-visible:bg-token-sidebar-surface-secondary focus-visible:outline-0">
-				<CollapseIcon />
-			</button>
+		<div className="flex h-14 items-center justify-end">
 			<button
 				onClick={() => router.push("/")}
 				className="h-10 rounded-lg px-2.5 text-token-text-secondary hover:bg-token-sidebar-surface-secondary focus-visible:bg-token-sidebar-surface-secondary focus-visible:outline-0"
