8a9b55b384cac8c73c10f8191ebcbf8e954cff41
Mavrick91 <ray.mavrick@gmail.com>
2024-05-20 11:42:23 +0800

    chore: Filter out blacklisted models in getModels action

--- src/actions/get-models.ts
+++ src/actions/get-models.ts
@@ -1,5 +1,22 @@
 "use server";
 
+const blackListedModels = [
+	"text-embedding-3-large",
+	"text-embedding-3-small",
+	"tts-1-hd-1106",
+	"tts-1-1106",
+	"tts-1-hd",
+	"dall-e-2",
+	"dall-e-3",
+	"gpt-3.5-turbo-instruct-0914",
+	"gpt-3.5-turbo-instruct",
+	"babbage-002",
+	"davinci-002",
+	"tts-1",
+	"whisper-1",
+	"text-embedding-ada-002",
+];
+
 export async function getModels() {
 	const res = await fetch("https://api.openai.com/v1/models", {
 		headers: {
@@ -8,7 +25,7 @@ export async function getModels() {
 	});
 	const data: Models = await res.json();
 
-	const modelsSorted = data.data.sort((a, b) => b.created - a.created);
+	const modelsSorted = data.data.sort((a, b) => b.created - a.created).filter((model) => !blackListedModels.includes(model.id));
 
 	return modelsSorted;
 }
