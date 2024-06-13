dc034ae7e8a5ddce820ae6911ae776551cbe1867
Mavrick91 <ray.mavrick@gmail.com>
2024-06-13 16:25:46 +0800

    refactor: Update modal styles for OpenAI key entry

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -5,36 +5,46 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import ChatMessages from "./_components/ChatMessages";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const Chat = ({ params }: NextPageProps) => {
-	const queryClient = useQueryClient();
-	const chatId = params.id;
-	const { data: session } = useSession();
-	const { data: conversations } = useQuery<SideBarConversation[]>({
-		queryKey: ["sideBarConversation", session?.user?.email],
-	});
-	const { data: firstMessage } = useQuery<SideBarConversation>({
-		queryKey: ["first-message"],
-	});
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const chatId = params.id;
+  const { data: session } = useSession();
+  const { data: conversations } = useQuery<SideBarConversation[]>({
+    queryKey: ["sideBarConversation", session?.user?.email],
+  });
+  const { data: firstMessage } = useQuery<SideBarConversation>({
+    queryKey: ["first-message"],
+  });
 
-	let conversation = conversations?.find((conversation) => conversation.id === chatId);
+  let conversation = conversations?.find(
+    (conversation) => conversation.id === chatId
+  );
 
-	if (firstMessage && !conversation) {
-		conversation = firstMessage;
-	}
+  if (firstMessage && !conversation) {
+    conversation = firstMessage;
+  }
 
-	useEffect(() => {
-		queryClient.setQueryData(["first-message"], null);
-	}, [queryClient]);
+  useEffect(() => {
+    queryClient.setQueryData(["first-message"], null);
 
-	return (
-		<div className="relative flex h-full flex-col">
-			<div className="flex h-0 grow flex-col items-center justify-center text-token-text-primary">
-				{conversation?.messages.length ? <ChatMessages conversation={conversation} /> : null}
-			</div>
-			<FormInput chatId={chatId} />
-		</div>
-	);
+    if (!conversation || !conversation.messages.length) {
+      router.push("/");
+    }
+  }, [conversation, queryClient, router]);
+
+  return (
+    <div className="relative flex h-full flex-col">
+      <div className="flex h-0 grow flex-col items-center justify-center text-token-text-primary">
+        {conversation?.messages.length ? (
+          <ChatMessages conversation={conversation} />
+        ) : null}
+      </div>
+      <FormInput chatId={chatId} />
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -4,20 +4,27 @@ import GoogleLogo from "@/components/svg/GoogleLogo";
 import { signIn } from "next-auth/react";
 
 const Login = () => {
-	return (
-		<div className="flex h-dvh flex-col items-center justify-center bg-white">
-			<div className="p-10">
-				<h1 className="text-4xl font-bold text-[#2d333a]">Welcome back</h1>
-			</div>
+  return (
+    <div className="flex h-dvh flex-col items-center justify-center bg-white">
+      <div className="p-10">
+        <h1 className="text-4xl font-bold text-[#2d333a]">Welcome back</h1>
+      </div>
 
-			<button className="social-btn" onClick={() => signIn("google")}>
-				<span className="social-logo-wrapper">
-					<GoogleLogo />
-				</span>
-				<span className="text-left">Continue with Google</span>
-			</button>
-		</div>
-	);
+      <button
+        className="social-btn"
+        onClick={() =>
+          signIn("google", {
+            callbackUrl: `${window.location.origin}/`,
+          })
+        }
+      >
+        <span className="social-logo-wrapper">
+          <GoogleLogo />
+        </span>
+        <span className="text-left">Continue with Google</span>
+      </button>
+    </div>
+  );
 };
 
 export default Login;
